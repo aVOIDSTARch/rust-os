a73588c2fdb6f2cb8bf82868bd58600a6f4b5220
louisc <lcasine@my.wgu.edu>
2026-05-09 12:04:24 -0400

    implemented new writer in start function

--- crusty_os/src/main.rs
+++ crusty_os/src/main.rs
@@ -5,14 +5,17 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
 
+
+
+// this function is the entry point, since the linker looks for a function named `_start` by default
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // write the string to the VGA text buffer at 0xb8000
-    vga_buffer::print_something();
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
+    write!(vga_buffer::WRITER.lock(), "The answer to the question of life, the universe and everything is {}.", 42).unwrap();
+    // this function must never return, since there is no operating system to return to
     loop {}
 }
 
