f2ec2bf80fa741eeebc5522bb5c2b8869b7e493d
louisc <lcasine@my.wgu.edu>
2026-05-09 12:13:22 -0400

    updated start using the new simpler println macro

--- crusty_os/src/main.rs
+++ crusty_os/src/main.rs
@@ -12,9 +12,7 @@ mod vga_buffer;
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // write the string to the VGA text buffer at 0xb8000
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
-    write!(vga_buffer::WRITER.lock(), "The answer to the question of life, the universe and everything is {}.", 42).unwrap();
+    println!("Hello World{}", "!");
     // this function must never return, since there is no operating system to return to
     loop {}
 }
