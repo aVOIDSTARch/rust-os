823170c9ab890eb3a620341d21b2fd6b81402620
louisc <lcasine@my.wgu.edu>
2026-05-09 14:00:01 -0400

    implemented test support

--- crusty_os/src/main.rs
+++ crusty_os/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
@@ -22,4 +24,12 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
