45fd8d717d107af56d4671c3b86c899610d98696
louisc <lcasine@my.wgu.edu>
2026-05-12 10:38:33 -0400

    handling several issues with testing environment

--- crusty_os/src/main.rs
+++ crusty_os/src/main.rs
@@ -7,6 +7,7 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 
 // this function is the entry point, since the linker looks for a function named `_start` by default
@@ -14,7 +15,7 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     // write the string to the VGA text buffer at 0xb8000
     println!("Hello World{}", "!");
-    
+
     // invoke the test runner if we're running tests
     #[cfg(test)]
     test_main();
@@ -24,23 +25,54 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// This function is called on panic.
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    // exit QEMU with code 0 if the tests passed
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
+}
+
+// QEMU exits with code 0 if the first argument to `isa-debug-exit` is 1, and with code 1 if it's 2.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
