4fce514194227e94d85c75f25551c6c1c7fc25d5
louisc <lcasine@my.wgu.edu>
2026-05-16 14:42:51 -0400

    integrated the test code into the start function

--- crusty_os/src/main.rs
+++ crusty_os/src/main.rs
@@ -14,9 +14,14 @@ use crusty_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    crusty_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
